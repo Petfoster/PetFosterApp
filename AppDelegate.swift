bb0504a01d3b8ec738854dd6c184c522cf6d1c05
Nick Flores <53841083+sir-nik10@users.noreply.github.com>
2022-04-11 16:55:04 -0700

    Set Up Parse with Correct API keys
    AppDelegate points to the Parse DataBase Class on my Account. Feed View Controller Initialized the class "Listing" with a column of "author"

---
+++ AppDelegate.swift
@@ -0,0 +1,49 @@
+//
+//  AppDelegate.swift
+//  PetFoster
+//
+//  Created by Niko Holbrook on 4/6/22.
+//
+
+import UIKit
+import Parse
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                        $0.applicationId = "E9LmmkmwKJoIgatJZ9fazMfruT2D7HFKzZSDdw0N" // <- UPDATE
+                        $0.clientKey = "1lPtyLdNHqWctHgW22uOHJJByLRjSr9fJbEbqc4g" // <- UPDATE
+                        $0.server = "https://parseapi.back4app.com"
+                }
+                Parse.initialize(with: parseConfig)
+                
+                // --- end copy
+
+                return true
+            }
+    
+    
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
